e942131d2482a5577e200c243b9f0b89e6794a11
Bob J <bobjas777@hotmail.com>
2021-07-08 17:38:30 -0700

    simple sample of slicing that could be applied to encryption

--- slicing.py
+++ slicing.py
@@ -2,4 +2,14 @@ parrot="Norwegian Blue"
 print(parrot[0:9]+" "+parrot[10:])
 print(parrot[::1])
 print(parrot[::-1])
+print("=========================")
+print(" Very simple encryption ")
+text_to_encrypt=input("Enter a text to encrypt:  ")
+encrypted_text=text_to_encrypt[::-1]
+print(f"Encrypted text: {encrypted_text} ")
+decrypted_text=encrypted_text[::-1]
+if decrypted_text==text_to_encrypt:
+    print(f" Decrypted text: {decrypted_text}")
+else:
+    print("Error during decryption")
 
